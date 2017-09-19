80df234c2898d6e92d35a96c0c9e094fb387e2aa
Null <hetaoos@gmail.com>
2017-09-19 23:10:27 +0800

    更新为 .NET Standard 2.0

--- Baidu.Yingyan/Entity/EntityApi.cs
+++ Baidu.Yingyan/Entity/EntityApi.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.IO;
-using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Formatting;
-using System.Net.Http.Headers;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Baidu.Yingyan.Entity
--- Baidu.Yingyan/Fence/FenceApi.cs
+++ Baidu.Yingyan/Fence/FenceApi.cs
@@ -1,14 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Formatting;
-using System.Net.Http.Headers;
-using System.Text;
 using System.Threading.Tasks;
-using System.Web;
 
 namespace Baidu.Yingyan.Fence
 {
